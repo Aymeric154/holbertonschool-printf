9c0f48205f01673d554b5564f65e6fb4bd3d370e
Aymeric154 <aymeric.gelle@outlook.fr>
2024-07-23 14:28:44 +0200

    try to check

--- test/printf.c
+++ test/printf.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include "main.h"
 
 /**
  * _printf - is a custom implementation of the standard printf function
@@ -12,5 +12,42 @@
 
 int _printf(const char *format, ...)
 {
-	
+	int i = 0, len = 0;
+	va_list arg;
+	int (*func)(va_list);
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(arg, format);
+
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '\0')
+				return (-1);
+
+			func = get_func(format[i]);
+			if (func)
+			{
+				len += func(arg);
+			} else
+			{
+				_putchar('%');
+				_putchar(format[i]);
+				len += 2;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			len++;
+		}
+		i++;
+	}
+
+	va_end(arg);
+	return (len);
 }
