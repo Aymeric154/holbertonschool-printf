732bd99f1da269183f73603bc509ed3c431e436a
Aymeric154 <aymeric.gelle@outlook.fr>
2024-07-23 11:34:46 +0200

    second commit with content

---
+++ test/main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *
+ *
+ */
+
+typedef struct printer {
+
+    char *spec;
+    int (*func)(va_list);
+
+} printer_t;
+
+int (*get_func(char conv_spec))(va_list);
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_s(va_list arg);
+int print_c(va_list arg);
+int print_percent(va_list arg);
+
+#endif
---
+++ test/printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+int _printf(const char *format, ...)
+{}
